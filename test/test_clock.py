70db9298adb498f5de6afd0da4c6c3288c5721b2
Ian Pozella <ian.pozella@gmail.com>
2021-01-03 10:59:05 +0000

    Fix style to match pep8
    Additionally includes adding a requirments file.

---
+++ test/test_clock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+
+import unittest
+
+import clock
+
+
+class TestTime(unittest.TestCase):
+    """ Test hardness to drive all LEDs """
+
+    def setup(self):
+        self.clock = clock.Clock()
+
+    def test_past_the_hour(self):
+        self.clock.update_time() #TODO!
+
+    def test_to_the_hour(self):
+        self.clock.update_time()
+
+
+if __name__ == '__main__':
+    unittest.main()
